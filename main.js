2987eadf218e00fdfca0f66c0d7e73f4b98f004f
LloydChris <chrislmjohnson@gmail.com>
2017-02-05 11:01:09 -0600

    prototype of getting, caching, reading from cache of one weather source

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var redis = require('redis');
-
 
 app.use('/', require('./routes'));
 
@@ -13,10 +11,4 @@ app.use(function(req, res, next){
 
 var server = app.listen(8080, function(){
 	console.log("Listening on 8080");
-    });
-
-var client = redis.createClient(6379, '127.0.0.1', {no_ready_check: true});
-
-client.on('connect', function() {
-	console.log('Connected to Redis');
     });
--- src/resources/resources.js
+++ src/resources/resources.js
@@ -1,24 +1,50 @@
 var request = require('request');
 var OpenWeatherKey = require('./openWeatherMap.js');
+var redis = require('redis');
+
+var cache = redis.createClient(6379, '127.0.0.1', {no_ready_check: true});
+
+cache.on('connect', function() {
+	console.log('Connected to Redis');
+    });
 
 var resources = {
     OpenWeatherByCoordinates: function (coordinates, callback){
-	var parameters = {
-	    lat: coordinates.latitude,
-	    lon: coordinates.longitude,
-	    APPID: OpenWeatherKey.key
-	};
-	var url = 'http://api.openweathermap.org/data/2.5/forecast';
-
-	request({url:url, qs:parameters}, function(err, response, body){
-		if(err){
-		    return err
+	var key = JSON.stringify(coordinates);
+	cache.get(key, function(err, reply){
+		
+		if(reply){
+		    console.log('Got Cache');
+		    callback(JSON.parse(reply));
+		} else {
+		    console.log('Empty');
+		    Get(key, coordinates, callback);
 		}
-		var result = ParseResponse(body);
-		callback(result);
-	    });
+	    }); 
     }
 };
+
+function Get(key, coordinates, callback){
+    var parameters = {
+	lat: coordinates.latitude,
+	lon: coordinates.longitude,
+	APPID: OpenWeatherKey.key
+    };
+    var url = 'http://api.openweathermap.org/data/2.5/forecast';
+    
+    request({url:url, qs:parameters}, function(err, response, body){
+	    if(err){
+		console.log(err);
+		return err;
+	    }
+	    var result = ParseResponse(body);
+	    
+	    cache.set(key, JSON.stringify(result));
+	    cache.expire(key, 3);
+	    console.log('cached');
+	    callback(result);
+	});   
+}
 
 function ParseResponse(body){
     var jsonBody = JSON.parse(body);
