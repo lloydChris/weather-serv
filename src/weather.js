15e9dafdae9696c868cf10f3bda193485fef31e8
LloydChris <chrislmjohnson@gmail.com>
2017-02-22 21:44:06 -0600

    Calling 2 datasources async style and agregating results

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,6 @@
 var utilities = require('../src/Utilities.js');
 var resources = require('../src/resources/resources.js');
+var async = require('async');
 
 var weather = {
     get: function(req, res){
@@ -8,17 +9,27 @@ var weather = {
     getByCoordinates: function(req, res){
 	var coordinates = utilities.parseCoordinates(req.params.coordinates);
 	
-	resources.ApixuByCoordinates(coordinates, function(result){
-		res.statusCode = 200;
-		res.end(JSON.stringify(result));
-	    });
-	/*
-	resources.OpenWeatherByCoordinates(coordinates, function(result){
-		res.statusCode = 200;
-		res.end(JSON.stringify(result));
-	    });
-	*/
+	var asyncTasks = [];
 
+	asyncTasks.push(function(callback){
+		resources.ApixuByCoordinates(coordinates, function(result){
+			res.statusCode = 200;
+			callback(null, result);
+		    });
+	    });
+
+	asyncTasks.push(function(callback){
+		resources.OpenWeatherByCoordinates(coordinates, function(result){
+			res.statusCode = 200;
+			callback(null, result);
+		    })});
+
+	async.parallel(asyncTasks, function(err, results){
+		if (err){
+		    res.end(err.toString());		    
+		}
+		res.end(JSON.stringify(results));
+	    });
     },
     getByCity: function(req, res){
 	res.end(req.params.city);
